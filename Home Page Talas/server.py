46f08cba82923821cff6b6098e2806974ef6b2e3
Mohammad Daffa Setiawan <102302805+warlock0401@users.noreply.github.com>
2024-08-19 11:17:41 +0700

    Add files via upload

--- Home Page Talas/server.py
+++ Home Page Talas/server.py
@@ -1,17 +1,19 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import json
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+# Dapatkan path absolut ke file trending_topics.json
+file_path = os.path.join(os.path.dirname(__file__), 'trending_topics.json')
 
 @app.route('/trending-topics', methods=['GET'])
 def get_trending_topics():
-    with open('trending_topics.json', 'r') as f:
+    with open(file_path, 'r') as f:
         trending_topics = json.load(f)
     return jsonify(trending_topics)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
